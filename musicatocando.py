1cfef13774dfd8775a23119356919fab7a099c44
be-actress <bia.machado2403@gmail.com>
2021-06-03 00:35:55 -0300

    ve se vai

---
+++ musicatocando.py
@@ -0,0 +1,9 @@
+from playsound import playsound             #classe que faz a mágica da música
+
+playsound("C:/Users/Desktop Positivo/Documents/kaduapp/maanof.wav")
+#colocando a mágica da música em prática
+# por uma razão que ainda NÃO SEI, só funciona com wav
+
+
+
+
